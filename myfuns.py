2e04f446c7fcb6550a32bedb95eb02eedd960871
nickchun <nickchun@uw.edu>
2023-12-11 16:32:24 -0800

    add ratings data

--- myfuns.py
+++ myfuns.py
@@ -1,19 +1,24 @@
 import pandas as pd
 import requests
 
-# Define the URL for movie data
-myurl = "https://liangfgithub.github.io/MovieData/movies.dat?raw=true"
+# Define the URLs for movie data
+movies_url = "https://liangfgithub.github.io/MovieData/movies.dat?raw=true"
+ratings_url = "https://liangfgithub.github.io/MovieData/ratings.dat?raw=true"
 
 # Fetch the data from the URL
-response = requests.get(myurl)
+movies = requests.get(movies_url)
+ratings = requests.get(ratings_url)
 
 # Split the data into lines and then split each line using "::"
 movie_lines = response.text.split('\n')
 movie_data = [line.split("::") for line in movie_lines if line]
+ratings_lines = response.text.split('\n')
+ratings_data = [line.split("::") for line in ratings_lines if line]
 
-# Create a DataFrame from the movie data
+# Create DataFrames from the movie data
 movies = pd.DataFrame(movie_data, columns=['movie_id', 'title', 'genres'])
 movies['movie_id'] = movies['movie_id'].astype(int)
+ratings = pd.DataFrame(ratings_data, columns=['user_id', 'movie_id', 'rating', 'timestamp'], dtype=int)
 
 genres = list(
     sorted(set([genre for genres in movies.genres.unique() for genre in genres.split("|")]))
